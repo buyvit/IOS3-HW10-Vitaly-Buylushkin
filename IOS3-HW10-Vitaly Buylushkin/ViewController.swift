0c4795ea0695153b4f3c5708fc31de0e8f1d2237
Vitaly Buylushkin <vitalybuylushkin@yandex.ru>
2021-09-11 16:37:39 +0300

    Initial Commit

---
+++ IOS3-HW10-Vitaly Buylushkin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS3-HW10-Vitaly Buylushkin
+//
+//  Created by Vitaly Buylushkin on 11.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
